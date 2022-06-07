b31c9c44c3685de9bc6b51f34b624d7efd1bff12
zhenquan.qiu <gnsyxiang@163.com>
2022-06-07 13:39:56 +0800

    fix: set right display for nvim-tree

--- lua/conf/nvim-tree.lua
+++ lua/conf/nvim-tree.lua
@@ -4,14 +4,11 @@ require("nvim-tree").setup(
 	{
 	-- 视图
         view = {
-            -- 宽度
             width = 30,
-            -- 高度
             height = 30,
-            -- 隐藏顶部的根目录显示
             hide_root_folder = false,
-            -- 自动调整大小
-            auto_resize = true
+            auto_resize = true,
+	    side = "right",
         }
 	}
 )
